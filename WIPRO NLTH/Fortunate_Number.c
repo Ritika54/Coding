e4ac9214668b1bbad9ca909510698cd762f0f820
Ritika54 <ritika05042000@gmail.com>
2021-08-14 01:35:03 +0530

    Update Fortunate_Number.c

--- WIPRO NLTH/Fortunate_Number.c
+++ WIPRO NLTH/Fortunate_Number.c
@@ -20,8 +20,14 @@ fortunate would be here 7,since (30+7) is a prime number
 
 int prime(int a){
     
+    if(a==2)
+        return 1;
+        
+    if(a%2==0)
+        return 0;
+
     int sqr=sqrt(a);
-    for(int i=2;i<=sqr;i++){
+    for(int i=3;i<=sqr;i+=2){
         if(a%i == 0)
             return 0;
     }
@@ -31,13 +37,25 @@ int prime(int a){
 
 int product_prime(int a){
 
-    int num=2,product=1;
+    if(a==0)
+        return 0;
+    if(a==1)
+        return 2;
+
+    int num=3,product=2;
+    /*
+    Since num here is 3 and we are incrementing it by 2. 
+    This means we are only checking for odd numbers, because other than 2, 
+    no even number is prime.
+    That's why we have already considered 2 in the product if a>1
+    */
+   --a;
     while(a){
         if(prime(num)){
             product*=num;
             a--;
         }
-        num++;
+        num+=2;        
     }
     
     return product;
@@ -45,8 +63,8 @@ int product_prime(int a){
 
 int main(){
 
-    int a,i=2;
-    scanf("%d",&a);
+    unsigned int a,i=2;
+    scanf("%u",&a);//since 'a' is a count of number for which we have to product,so cannot be negative 
     
     a=product_prime(a);
     
