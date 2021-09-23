9ec68128c290102ed49f15d7fd6998ed78e55186
Ritika54 <ritika05042000@gmail.com>
2021-09-23 15:26:31 +0530

    Update Series2.c

--- WIPRO NLTH/Series2.c
+++ WIPRO NLTH/Series2.c
@@ -3,7 +3,7 @@ Find Nth term of following series
 3 3 0 6 1 9 1 9 2 27 3 12 5 81 8 15 13 243.....
 here this series is combination of 2 series
 I  = 3 6 9 12 15....(AP)(1 4 8 12 16... terms)
-II = 3 9 27 81....(GP)(2 6 10 14 18...terms)
+II = 3 9 27 81 243 729....(GP)(2 6 10 14 18 22...terms)
 III= 0 1 1 2 3 5 8 13 (Fibonacci Series)(3 5 7 9 11 13...terms)
 So,
 here we can deduce that if we are given any 
@@ -19,12 +19,13 @@ here we can deduce that if we are given any
 
 (ii)multiple of 2 but not 4
    let, n=22
-   term=(int) (n/2)
-   => term = 3
-   => 3rd term of II AP
+   term=(int) (n/4)
+   => term = 5+1
+   => 6th term of II AP
    '.' we know that 
    Tn=a1*(r)^(n-1)
-   Tn=3*(3)^(3-1) = 27
+   here we can write Tn as a1*(3)^((n/4)+1-1) => 3^((n/4)+1)
+   Tn=3*(3)^(6-1) = 729
 
 (iii)odd terms except 1
    let, n=9
@@ -63,7 +64,7 @@ int main(){
         Tn=(12+3*n)/4;
     }
     else if(n%2==0){//II series
-
+        Tn=pow(3,((int)(n/4))+1);
     }
     else{//III series
         n=n/2;
