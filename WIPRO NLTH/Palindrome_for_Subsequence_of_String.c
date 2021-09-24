3a3a70674d4356051fd50bbad870c07611946936
Ritika54 <ritika05042000@gmail.com>
2021-09-24 22:50:04 +0530

    Update Palindrome_for_Subsequence_of_String.c

--- WIPRO NLTH/Palindrome_for_Subsequence_of_String.c
+++ WIPRO NLTH/Palindrome_for_Subsequence_of_String.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<string.h>
-int palindrome(char *str,int start,int end){
-    for(int i=start,j=end;i<j;i++,j--){
-        if(str[i]!=str[j])
-            return 0;
-    }
-    return 1;
+int palindrome(char *str,int i,int j)
+{
+        while(i<j)
+                if(str[i++] != str[j--])
+                        return 0;
+        return 1; //palindrome
 }
 void subSequence(char *str){
     int len =strlen(str);
     int max=len-1;//Maximum length of Substring
-    for(int i=1;i<=max;i++){//i=length of subsequence
+    for(int i=2;i<=max;i++){//i=length of subsequence
         for(int j=0;j<=len-i;j++){//j=start index
             if(palindrome(str,j,j+i-1)){        
                 for(int k=0;k<i;k++){
